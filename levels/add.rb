2e0c563b3567752cc786cd8077e65ad365254468
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2013-10-11 20:18:14 +0200

    Minor rephrase of level 1 description.

--- levels/add.rb
+++ levels/add.rb
@@ -1,18 +1,21 @@
 difficulty 1
-description "There is a file in your folder called README, you should add it to your staging area
+description "There is a file in your folder called README, you should add it to your repository.
 Note: You start each level with a new repo. Don't look for files from the previous one"
 
 setup do
   repo.init
-  FileUtils.touch("README")
+  FileUtils.touch('README')
 end
 
 solution do
-  return false unless repo.status.files.keys.include?("README")
-  return false if repo.status.files["README"].untracked
+  # In mercurial-ruby it seems difficult to find not committed adds.
+  # Note thtat string comparison assumes that hg stat is not i18ned.
+  # TODO If repo.shell does not set e.g. LC_ALL (or windows equ.), do it.
+  return false unless repo.shell.run('hg stat').strip == 'A README'
+  #return false if repo.status.files["README"].untracked
   true
 end
 
 hint do
-  puts "You can type `git` in your shell to get a list of available git commands"
+  puts 'You can type `hg` in your shell to get a list of available hg commands'
 end
