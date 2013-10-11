cf968e78898e5c5b5040934d5271ea85f041c13f
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2013-10-11 20:21:23 +0200

    Replaced git with hg.

--- levels/init.rb
+++ levels/init.rb
@@ -1,10 +1,10 @@
 difficulty 1
-description "A new directory, git_hug, has been created; initialize an empty repository in it"
+description "A new directory, hg_hug, has been created; initialize an empty repository in it"
 
 solution do
   repo.valid?
 end
 
 hint do
-  puts "You can type `git` in your shell to get a list of available git commands"
+  puts "You can type `hg` in your shell to get a list of available hg commands"
 end
